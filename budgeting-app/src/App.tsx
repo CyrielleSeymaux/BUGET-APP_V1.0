a40026367467664c9d8d098969e8a7018288affd
Cyrielle Seymaux <cyrielle.seymaux@gmail.com>
2026-01-25 18:18:09 -0500

    Debug

--- budgeting-app/src/App.tsx
+++ budgeting-app/src/App.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
-import IncomeForm from './components/IncomeForm';
-import ExpenseForm from './components/ExpenseForm';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Dashboard} />
-        <Route path="/income" component={IncomeForm} />
-        <Route path="/expenses" component={ExpenseForm} />
-      </Switch>
-    </Router>
+    <Dashboard />
   );
 };
 
